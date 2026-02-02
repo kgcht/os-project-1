3948b6072e98ac7cb13ed966c33250c35bfd0e9c
Kayla Gaynor <kgcht@umsystem.edu>
2026-02-02 13:27:35 -0600

    Implement command-line argument validation in user.c
    Add main function to user.c with command-line argument check

---
+++ user.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+
+int main(int argc, char *argv[]) {
+  //Check if I got the required command-line argument
+  if (argc != 2) {
+    fprintf(stderr, "Usage: %s <number_of_iterations>\n", argv[0]);
+    return 1;
+  }
